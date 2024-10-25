90982f8774185abf9c0768cc5c01c67d680ee1f4
3lreymon <ramonbatidas@gmail.com>
2024-10-25 15:12:02 -0700

    Primer commit - Agregando el proyecto de Geolocalización sin API

---
+++ js/locationpicker.jquery.js
@@ -0,0 +1,41 @@
+﻿(function ($) {
+    function GMapContext(domElement, options) {
+       
+        this.domElement = domElement;
+        this.options = options;
+
+       
+        this.location = null;
+        this.radius = options.radius || 100;
+        this.locationName = options.locationName || '';
+        this.addressComponents = {};
+
+        
+        console.log("Mapa inicializado sin Google Maps. Opciones:", options);
+    }
+
+    var GmUtility = {
+       
+        drawCircle: function () {
+            console.log("Función drawCircle: lógica para dibujar un círculo aquí.");
+        },
+        setPosition: function () {
+            console.log("Función setPosition: lógica para establecer posición aquí.");
+        },
+        updateLocationName: function () {
+            console.log("Función updateLocationName: lógica para actualizar nombre de ubicación aquí.");
+        }
+    };
+
+    $.fn.locationpicker = function (options, params) {
+        return this.each(function () {
+            var $target = $(this);
+            var instance = new GMapContext(this, options);
+
+            
+            console.log("Location Picker activado en:", $target);
+
+
+        });
+    };
+})(jQuery);
